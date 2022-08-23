ff0253bea27c34cc4e43a773fec640c6084a9731
Rodrigo Hashimoto <Rodrigo.Hashimoto@kyndryl.com>
2022-08-23 19:59:29 +0000

    Added  the second tag

--- main.tf
+++ main.tf
@@ -139,6 +139,10 @@ resource "azurerm_virtual_machine" "catapp" {
     value = "devops"
   }
 
+  tags = {
+    Billable = true
+  }
+
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
 }
